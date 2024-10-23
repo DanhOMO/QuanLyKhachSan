a47bb9fb3117ab63584aea88e92407eb06ff5731
Nguyen Cong Danh <125718628+DanhOMO@users.noreply.github.com>
2024-10-23 21:25:57 +0700

    Add files via upload

---
+++ CoSoDuLieu_v1.sql
@@ -0,0 +1,432 @@
+﻿use master
+drop database QuanLyKhachSan
+create database QuanLyKhachSan
+use QuanLyKhachSan
+-- Bảng DichVu
+CREATE TABLE DichVu (
+    maDichVu VARCHAR(50) PRIMARY KEY,
+    tenDichVu VARCHAR(100),
+    moTa TEXT,
+    giaDichVu float
+);
+-- Bảng LoaiPhong
+CREATE TABLE LoaiPhong (
+    maLoaiPhong VARCHAR(50) PRIMARY KEY,
+    tenLoaiPhong VARCHAR(100),
+    moTa TEXT,
+    soLuongNguoi INT,
+    giaThuePhong float
+);
+
+
+CREATE TABLE ThietBi (
+    maThietBi VARCHAR(50) PRIMARY KEY,
+    tenThietBi VARCHAR(100),
+    trangThai VARCHAR(50),
+);
+-- Bảng KhuVuc
+CREATE TABLE KhuVuc (
+    maKhuVuc VARCHAR(50) PRIMARY KEY,
+    tenKhuVuc VARCHAR(100),
+    moTa TEXT
+);
+
+
+-- Bảng KhachHang
+CREATE TABLE KhachHang (
+    maKhachHang VARCHAR(50) PRIMARY KEY,
+    tenKhachHang VARCHAR(100),
+    soDienThoai VARCHAR(15),
+    gioiTinh varchar(50),
+    diaChi TEXT,
+    ngaySinh DATE,
+    cccd VARCHAR(50),
+    email VARCHAR(100),
+);
+
+-- Bảng Voucher
+CREATE TABLE Voucher (
+    maVoucher VARCHAR(50) PRIMARY KEY,
+    tenVoucher VARCHAR(100),
+    giaGiam float,
+    ngayBatDau DATE,
+    ngayKetThuc DATE
+);
+-- Bảng Phong
+CREATE TABLE Phong (
+    maPhong VARCHAR(50) PRIMARY KEY,
+    tenPhong NVARCHAR(50) ,
+    trangThaiPhong varchar(50),
+    maLoaiPhong VARCHAR(50),
+	maKhuVuc VARCHAR(50),
+    FOREIGN KEY (maLoaiPhong) REFERENCES LoaiPhong(maLoaiPhong),
+	FOREIGN KEY (maKhuVuc) REFERENCES KhuVuc(maKhuVuc),
+	CONSTRAINT CHK_TrangThaiPhong CHECK (trangThaiPhong IN ('TRONG', 'DADAT', 'BAOTRI', 'DANGDONDEP')),
+);
+CREATE TABLE Phong_ThietBi (
+    maThietBi VARCHAR(50),
+    maPhong VARCHAR(50),
+    PRIMARY KEY (maPhong, maThietBi),
+    FOREIGN KEY (maThietBi) REFERENCES ThietBi(maThietBi),
+    FOREIGN KEY (maPhong) REFERENCES Phong(maPhong)
+);
+-- Bảng ChiTietHoaDon
+CREATE TABLE ChiTietHoaDon (
+    maChiTietHoaDon VARCHAR(50) PRIMARY KEY,
+    ngapLapHoaDon DATE,
+    giaDatHang float
+);
+CREATE TABLE LichSuDatPhong (
+    maChiTietHoaDon VARCHAR(50),
+    maPhong VARCHAR(50),
+	soLuong INT,
+	thoiGianDatPhong datetime,
+    PRIMARY KEY (maChiTietHoaDon, maPhong),
+    FOREIGN KEY (maChiTietHoaDon) REFERENCES ChiTietHoaDon(maChiTietHoaDon),
+    FOREIGN KEY (maPhong) REFERENCES Phong(maPhong)
+);
+CREATE TABLE LichSuDatDichVu (
+    maChiTietHoaDon VARCHAR(50),
+    maDichVu VARCHAR(50),
+	thoiGianDatDichVu datetime,
+	 soLuongDatHang INT,
+    PRIMARY KEY (maChiTietHoaDon, maDichVu),
+    FOREIGN KEY (maChiTietHoaDon) REFERENCES ChiTietHoaDon(maChiTietHoaDon),
+    FOREIGN KEY (maDichVu) REFERENCES DichVu(maDichVu)
+);
+
+
+
+
+
+-- Bảng LoaiNhanVien
+CREATE TABLE LoaiNhanVien (
+    maLoaiNhanVien VARCHAR(50) PRIMARY KEY,
+    tenLoaiNhanVien VARCHAR(100)
+);
+
+-- Bảng NhanVien
+CREATE TABLE NhanVien (
+    maNhanVien VARCHAR(50) PRIMARY KEY,
+    tenNhanVien VARCHAR(100),
+    soDienThoai VARCHAR(15),
+    gioiTinh varchar(50),
+    diaChi TEXT,
+    ngaySinh DATE,
+    email VARCHAR(100),
+    maLoaiNhanVien VARCHAR(50),
+    trangThai varchar(50),
+    FOREIGN KEY (maLoaiNhanVien) REFERENCES LoaiNhanVien(maLoaiNhanVien),
+
+);
+CREATE TABLE CaLamViec(
+	maCaLamVien varchar(50) PRIMARY KEY,
+	tenCaLamViec varchar(50) ,
+	ngayLamviec date,
+	tongTienTrongCa float,
+	maNhanVien varchar(50),
+	FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien)
+	
+)
+-- Bảng HoaDon
+CREATE TABLE HoaDon (
+    maHoaDon VARCHAR(50) PRIMARY KEY,
+    ngayLapHoaDon DATE,
+    maNhanVien VARCHAR(50),
+    maVoucher VARCHAR(50),
+	maKhachHang VARCHAR(50),
+	maChiTietHoaDon VARCHAR(50),
+    VAT float,
+    trangThai bit,
+	checkIN BIT,
+    checkOUT BIT,
+	datCoc float,
+	tienPhat float,
+	tongTien float,
+    FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien),
+    FOREIGN KEY (maVoucher) REFERENCES Voucher(maVoucher),
+	FOREIGN KEY (maKhachHang) REFERENCES KhachHang(maKhachHang),
+	FOREIGN KEY (maChiTietHoaDon) REFERENCES ChiTietHoaDon(maChiTietHoaDon)
+);
+
+-- Bảng TaiKhoan
+CREATE TABLE TaiKhoan (
+    tenTaiKhoan VARCHAR(50) PRIMARY KEY,
+    matKhau VARCHAR(100),
+    trangThai VARCHAR(50),
+	maNhanVien VARCHAR(50),
+	FOREIGN KEY (maNhanVien) REFERENCES NhanVien(maNhanVien)
+);
+
+
+-- Ràng Buộc 
+ALTER TABLE NhanVien
+add CONSTRAINT chk_maNV_Format
+CHECK (
+    maNhanVien LIKE 'NV[0-9][0-9][0-9]'
+);
+ALTER TABLE NhanVien
+ADD CONSTRAINT chk_tenNhanVien_Format
+CHECK (
+    tenNhanVien IS NOT NULL AND -- Không được NULL
+    tenNhanVien NOT LIKE '%[^A-Za-z ]%' AND -- Không chứa ký tự số và ký tự đặc biệt (chỉ cho phép chữ cái và dấu cách)
+    tenNhanVien LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenNhanVien NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenNhanVien))) = LEN(tenNhanVien) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+
+ALTER TABLE NhanVien
+ADD CONSTRAINT chk_TuoiHopLe
+CHECK (
+    YEAR(GETDATE()) - YEAR(ngaySinh) >= 15
+);
+
+ALTER TABLE NhanVien
+ADD CONSTRAINT chk_email_Format
+CHECK (
+    email IS NOT NULL AND -- Không được NULL
+    email LIKE '%@gmail.com' AND -- Có đuôi @gmail.com
+    email NOT LIKE '%[^A-Za-z0-9@._%+-]%' -- Chỉ chứa chữ cái, số, và các ký tự đặc biệt hợp lệ
+);
+
+ALTER TABLE LoaiNhanVien
+add CONSTRAINT chk_maLoaiNhanVien_Format
+CHECK (
+    maLoaiNhanVien LIKE 'MLNV[0-9][0-9]' 
+);
+ALTER TABLE LoaiNhanVien
+ADD CONSTRAINT chk_tenLoaiNhanVien_Format
+CHECK (
+    tenLoaiNhanVien IS NOT NULL AND -- Không được NULL
+    tenLoaiNhanVien NOT LIKE '%[^A-Za-z ]%' AND -- Không chứa ký tự số và ký tự đặc biệt (chỉ cho phép chữ cái và dấu cách)
+    tenLoaiNhanVien LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenLoaiNhanVien NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenLoaiNhanVien))) = LEN(tenLoaiNhanVien) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+-- Dung trigger rang buoc mat khau
+
+
+ALTER TABLE Voucher
+ADD CONSTRAINT chk_voucher_Format
+CHECK (
+    maVoucher LIKE 'VC[0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9]'
+);
+
+ALTER TABLE Voucher
+ADD CONSTRAINT chk_tenVoucher_Format
+CHECK (
+    tenVoucher IS NOT NULL AND -- Không được NULL
+    tenVoucher NOT LIKE '%[^A-Za-z0-9% ]%' AND -- Cho phép chữ cái, số, dấu phần trăm và dấu cách
+    tenVoucher LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenVoucher NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenVoucher))) = LEN(tenVoucher) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+
+
+ALTER TABLE Voucher
+ADD CONSTRAINT chk_giamGia_Format
+CHECK (
+    giaGiam > 0 AND -- Giảm giá phải lớn hơn 0
+    giaGiam IS NOT NULL -- Không được NULL
+);
+
+
+
+ALTER TABLE Voucher
+ADD CONSTRAINT chk_ngayKT_Format
+CHECK (
+
+    ngayKetThuc >= ngayBatDau -- Ngày kết thúc phải lớn hơn hoặc bằng ngày bắt đầu
+);
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT chk_maKhachHang_Format
+CHECK (
+    maKhachHang LIKE 'KH[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]'
+);
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT chk_tenKhachHang_Format
+CHECK (
+    tenKhachHang IS NOT NULL AND -- Không được NULL
+    tenKhachHang NOT LIKE '%[^A-Za-z ]%' AND -- Không chứa ký tự số và ký tự đặc biệt (chỉ cho phép chữ cái và dấu cách)
+    tenKhachHang LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenKhachHang NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenKhachHang))) = LEN(tenKhachHang) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT chk_cccd_Format
+CHECK (
+    LEN(cccd) = 10
+);
+
+
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT chk_TuoiHopLe_KhachHang
+CHECK (
+    YEAR(GETDATE()) - YEAR(ngaySinh) >= 15
+);
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT chk_email_Format_KhachHang
+CHECK (
+    email IS NOT NULL AND -- Không được NULL
+    email LIKE '%@gmail.com' AND -- Có đuôi @gmail.com
+    email NOT LIKE '%[^A-Za-z0-9@._%+-]%' -- Chỉ chứa chữ cái, số, và các ký tự đặc biệt hợp lệ
+);
+
+
+ALTER TABLE HoaDon
+ADD CONSTRAINT chk_maHoaDon_Format
+CHECK (
+    maHoaDon LIKE 'HD[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]' -- 8 ký tự sau 'HD' và 3 ký tự sau '-'
+);
+
+
+
+ALTER TABLE LoaiPhong
+ADD CONSTRAINT chk_maLoaiPhong_Format
+CHECK (
+    maLoaiPhong LIKE 'LP[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]' AND
+    maLoaiPhong IS NOT NULL AND
+    maLoaiPhong <> '' -- Không được rỗng
+);
+
+ALTER TABLE LoaiPhong
+ADD CONSTRAINT chk_tenLoaiPhong_Format
+CHECK (
+    tenLoaiPhong IS NOT NULL AND -- Không được NULL
+    tenLoaiPhong NOT LIKE '%[^A-Za-z ]%' AND -- Không chứa ký tự số và ký tự đặc biệt (chỉ cho phép chữ cái và dấu cách)
+    tenLoaiPhong LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenLoaiPhong NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenLoaiPhong))) = LEN(tenLoaiPhong) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+
+ALTER TABLE LoaiPhong
+ADD CONSTRAINT chk_soLuongNguoi
+CHECK (
+    soLuongNguoi IS NOT NULL AND -- Không được NULL
+    soLuongNguoi >= 0 AND       -- Phải lớn hơn hoặc bằng 0
+    soLuongNguoi <= 10          -- Phải nhỏ hơn hoặc bằng 10
+);
+
+ALTER TABLE LoaiPhong
+ADD CONSTRAINT chk_giaThuePhong
+CHECK (
+    giaThuePhong >= 0 AND  -- Phải lớn hơn hoặc bằng 0
+    giaThuePhong IS NOT NULL -- Không được NULL
+);
+
+ALTER TABLE DichVu
+ADD CONSTRAINT chk_maDichVu_Format
+CHECK (
+    maDichVu LIKE 'DV[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]' AND
+    maDichVu IS NOT NULL AND
+    maDichVu <> '' -- Không được rỗng
+);
+
+
+ALTER TABLE DichVu
+ADD CONSTRAINT chk_tenDichVu_Format
+CHECK (
+    tenDichVu IS NOT NULL AND -- Không được NULL
+    tenDichVu NOT LIKE '%[^A-Za-z ]%' AND -- Không chứa ký tự số và ký tự đặc biệt (chỉ cho phép chữ cái và dấu cách)
+    tenDichVu LIKE '[A-Z]%' AND -- Chữ đầu phải viết hoa
+    tenDichVu NOT LIKE '%  %' AND -- Không được có hai dấu cách liên tiếp
+    LEN(LTRIM(RTRIM(tenDichVu))) = LEN(tenDichVu) -- Không có khoảng trắng ở đầu hoặc cuối
+);
+
+
+ALTER TABLE DichVu
+ADD CONSTRAINT chk_giaDichVu
+CHECK (
+    giaDichVu >= 0 AND  -- Phải lớn hơn hoặc bằng 0
+    giaDichVu IS NOT NULL -- Không được NULL
+);
+
+ALTER TABLE HoaDon
+ADD CONSTRAINT chk_maHoaDon_Formatt
+CHECK (
+    maHoaDon LIKE 'HD[0-3][0-9][0-1][0-9][0-9][0-9]-[0-9][0-9][0-9]' AND
+    maHoaDon IS NOT NULL AND
+    maHoaDon <> '' -- Không được rỗng
+);
+
+
+
+ALTER TABLE ChiTietHoaDon
+add CONSTRAINT chk_maChiTietHoaDon_Format
+CHECK (
+    maChiTietHoaDon LIKE 'CTHD[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9]' AND
+    maChiTietHoaDon IS NOT NULL AND
+    maChiTietHoaDon <> '' -- Không được rỗng
+);
+
+ALTER TABLE LichSuDatPhong
+ADD CONSTRAINT chk_soLuongPhong
+CHECK (
+    soLuong > 0 AND  -- Phải lớn hơn 0
+    soLuong IS NOT NULL -- Không được NULL
+);
+
+ALTER TABLE LichSuDatDichVu
+ADD CONSTRAINT chk_soLuongDatHang
+CHECK (
+    soLuongDatHang > 0 AND  -- Phải lớn hơn 0
+    soLuongDatHang IS NOT NULL -- Không được NULL
+);
+
+
+
+ALTER TABLE ChiTietHoaDon
+ADD CONSTRAINT chk_giaDatHang
+CHECK (
+    giaDatHang >= 0 AND        -- Phải lớn hơn hoặc bằng 0
+    giaDatHang IS NOT NULL     -- Không được NULL
+);
+
+ALTER TABLE Phong
+ADD CONSTRAINT chk_maPhong_Format
+CHECK (
+    maPhong LIKE 'P[0-9][0-9][0-9]' AND          -- Đảm bảo định dạng PXXX
+    CAST(SUBSTRING(maPhong, 2, 3) AS INT) BETWEEN 1 AND 999 AND -- Đảm bảo số từ 1 đến 999
+    maPhong IS NOT NULL AND                        -- Không được NULL
+    maPhong <> ''                                  -- Không được rỗng
+);
+
+ALTER TABLE ThietBi
+ADD CONSTRAINT chk_maThietBi_Format
+CHECK (
+    maThietBi LIKE 'TB[0-9][0-9]' AND                     -- Đảm bảo định dạng TBXX
+               
+    maThietBi IS NOT NULL AND                              -- Không được NULL
+    maThietBi <> ''                                        -- Không được rỗng
+);
+
+ALTER TABLE KhuVuc
+ADD CONSTRAINT chk_maKhuVuc_Format
+CHECK (
+    maKhuVuc LIKE 'K[A-D]' AND       -- Đảm bảo định dạng KX với X là A, B, C, D
+    maKhuVuc IS NOT NULL AND         -- Không được NULL
+    maKhuVuc <> ''                   -- Không được rỗng
+);
+
+-- Rang buoc Trang Thai Nhan Vien
+ALTER TABLE NhanVien
+add CONSTRAINT CK_TrangThai_NV CHECK (TrangThai IN ('NGHI_PHEP', 'DANG_LAM_VIEC', 'NGHI_VIEC'))
+
+-- Rang buoc Trang Thai Thiet Bi
+ALTER TABLE ThietBi
+add CONSTRAINT CK_TrangThai_TB CHECK (TrangThai IN ('DANG_HOAT_DONG', 'KHONG_HOAT_DONG'))
+
+-- Rang buoc Trang Thai Tai Khoan
+ALTER TABLE TaiKhoan
+add CONSTRAINT CK_TrangThai_TK CHECK (TrangThai IN ('DANG_HOAT_DONG', 'KHONG_HOAT_DONG'))
+
+ALTER TABLE NhanVien
+ADD CONSTRAINT CK_GioiTin_NV CHECK (gioiTinh IN ('Nam', 'Nu', 'Khac'));
+
+ALTER TABLE KhachHang
+ADD CONSTRAINT CK_GioiTinh_KH CHECK (gioiTinh IN ('Nam', 'Nu', 'Khac'));
---
+++ DataMau_v1.sql
@@ -0,0 +1,154 @@
+﻿use QuanLyKhachSan
+-- Du lieu mau cho bang DichVu
+
+
+INSERT INTO DichVu (maDichVu, tenDichVu, moTa, giaDichVu) VALUES
+('DV00000001-001', 'Bua Sang', 'Bua sang buffet voi nhieu mon ngon', 150000),
+('DV00000002-002', 'Bua Trua', 'Bua trua buffet voi nhieu mon ngon', 120000),
+('DV00000003-003', 'Bua Chieu', 'Bua chieu buffet voi nhieu mon ngon', 200000),
+('DV00000004-004', 'Giat Ui', 'Dich vu giat ui nhanh chong', 50000),
+('DV00000005-005', 'Dua Don San Bay', 'Dich vu dua don tai san bay', 700000);
+
+-- Du lieu mau cho bang LoaiPhong
+INSERT INTO LoaiPhong (maLoaiPhong, tenLoaiPhong, moTa, soLuongNguoi, giaThuePhong) VALUES
+('LP00000001-001', 'Phong Don', 'Phong cho 1 nguoi', 1, 600000),
+('LP00000002-002', 'Phong Doi', 'Phong cho 2 nguoi', 2, 800000),
+('LP00000003-003', 'Phong Gia Dinh', 'Phong cho 4 nguoi', 4, 1200000),
+('LP00000004-004', 'Phong Sang Trong', 'Phong voi cac tien nghi cao cap', 2, 2000000);
+
+-- Du lieu mau cho bang ThietBi
+INSERT INTO ThietBi (maThietBi, tenThietBi, trangThai) VALUES
+('TB01', 'TV', 'DANG_HOAT_DONG'),
+('TB02', 'May lanh', 'KHONG_HOAT_DONG'),
+('TB03', 'Tu lanh', 'DANG_HOAT_DONG'),
+('TB04', 'Ban ghe', 'DANG_HOAT_DONG'),
+('TB05', 'TV', 'KHONG_HOAT_DONG'),
+('TB06', 'May lanh', 'DANG_HOAT_DONG'),
+('TB07', 'Tu lanh', 'KHONG_HOAT_DONG'),
+('TB08', 'Ban ghe', 'KHONG_HOAT_DONG');
+
+-- Du lieu mau cho bang KhuVuc
+INSERT INTO KhuVuc (maKhuVuc, tenKhuVuc, moTa) VALUES
+('KA', 'Khu Vuc A', 'Khu vuc A cua khach san'),
+('KB', 'Khu Vuc B', 'Khu vuc B cua khach san'),
+('KC', 'Khu Vuc C', 'Khu vuc C cua khach san');
+
+-- Du lieu mau cho bang KhachHang
+INSERT INTO KhachHang (maKhachHang, tenKhachHang, soDienThoai, gioiTinh, diaChi, ngaySinh, cccd, email) VALUES
+('KH0001-001', 'Nguyen Van A', '0123456789', 'Nam', 'Ha Noi', '1985-05-15', '1234567890', 'a@gmail.com'),
+('KH0002-002', 'Tran Thi B', '0987654321', 'Nu', 'Da Nang', '1990-08-22', '9876543210', 'b@gmail.com'),
+('KH0003-003', 'Le Van C', ' ', 'Nam', 'TP.HCM', '1988-03-12', '1112233440', 'c@gmail.com');
+
+-- Du lieu mau cho bang Voucher
+INSERT INTO Voucher (maVoucher, tenVoucher, giaGiam, ngayBatDau, ngayKetThuc) VALUES
+('VC2023101-01', 'Giam Gia 10%', 10, '2024-11-01', '2024-12-31'), -- Ngay bat dau da thay doi
+('VC2023102-03', 'Giam Gia 10%', 10, '2023-10-01', '2023-12-31'),
+('VC2023103-02', 'Giam Gia 20%', 20, '2024-10-01', '2024-11-30');
+
+-- Du lieu mau cho bang Phong
+INSERT INTO Phong (maPhong, tenPhong, trangThaiPhong, maLoaiPhong, maKhuVuc) VALUES
+-- Khu vuc KA
+('P001', 'Phong 101', 'TRONG', 'LP00000001-001', 'KA'),
+('P002', 'Phong 102', 'TRONG', 'LP00000001-001', 'KA'),
+('P003', 'Phong 103', 'TRONG', 'LP00000001-001', 'KA'),
+('P004', 'Phong 104', 'TRONG', 'LP00000001-001', 'KA'),
+('P005', 'Phong 105', 'DADAT', 'LP00000001-001', 'KA'),
+('P006', 'Phong 106', 'TRONG', 'LP00000001-001', 'KA'),
+('P007', 'Phong 107', 'BAOTRI', 'LP00000001-001', 'KA'),
+('P008', 'Phong 108', 'TRONG', 'LP00000001-001', 'KA'),
+('P009', 'Phong 109', 'DANGDONDEP', 'LP00000001-001', 'KA'),
+('P010', 'Phong 110', 'TRONG', 'LP00000001-001', 'KA'),
+
+-- Khu vuc KB
+('P011', 'Phong 201', 'TRONG', 'LP00000002-002', 'KB'),
+('P012', 'Phong 202', 'TRONG', 'LP00000002-002', 'KB'),
+('P013', 'Phong 203', 'DADAT', 'LP00000002-002', 'KB'),
+('P014', 'Phong 204', 'TRONG', 'LP00000002-002', 'KB'),
+('P015', 'Phong 205', 'TRONG', 'LP00000002-002', 'KB'),
+('P016', 'Phong 206', 'BAOTRI', 'LP00000002-002', 'KB'),
+('P017', 'Phong 207', 'TRONG', 'LP00000002-002', 'KB'),
+('P018', 'Phong 208', 'DANGDONDEP', 'LP00000002-002', 'KB'),
+('P019', 'Phong 209', 'TRONG', 'LP00000002-002', 'KB'),
+('P020', 'Phong 210', 'TRONG', 'LP00000002-002', 'KB'),
+
+-- Khu vuc KC
+('P021', 'Phong 301', 'TRONG', 'LP00000003-003', 'KC'),
+('P022', 'Phong 302', 'TRONG', 'LP00000003-003', 'KC'),
+('P023', 'Phong 303', 'TRONG', 'LP00000003-003', 'KC'),
+('P024', 'Phong 304', 'TRONG', 'LP00000003-003', 'KC'),
+('P025', 'Phong 305', 'DADAT', 'LP00000003-003', 'KC'),
+('P026', 'Phong 306', 'TRONG', 'LP00000003-003', 'KC'),
+('P027', 'Phong 307', 'BAOTRI', 'LP00000003-003', 'KC'),
+('P028', 'Phong 308', 'TRONG', 'LP00000003-003', 'KC'),
+('P029', 'Phong 309', 'DANGDONDEP', 'LP00000003-003', 'KC'),
+('P030', 'Phong 310', 'TRONG', 'LP00000003-003', 'KC');
+
+select * from Phong
+-- Du lieu mau cho bang ChiTietHoaDon
+INSERT INTO ChiTietHoaDon (maChiTietHoaDon, ngapLapHoaDon, giaDatHang) VALUES
+('CTHD000000001-001', '2024-10-14', 750000),
+('CTHD000000002-002', '2024-10-15', 850000),
+('CTHD000000003-003', '2024-10-16', 950000),
+('CTHD000000004-004', '2024-10-17', 600000),
+('CTHD000000005-005', '2024-10-18', 1200000);
+
+-- Chen du lieu vao bang LichSuDatPhong
+INSERT INTO LichSuDatPhong (maChiTietHoaDon, maPhong, soLuong, thoiGianDatPhong) VALUES
+('CTHD000000001-001', 'P003', 1, '2024-10-14 14:00:00'),
+('CTHD000000001-001', 'P004', 1, '2024-10-14 14:00:00'),
+('CTHD000000002-002', 'P013', 1, '2024-10-15 12:00:00'),
+('CTHD000000003-003', 'P022', 1, '2024-10-16 10:00:00'),
+('CTHD000000004-004', 'P023', 1, '2024-10-17 11:00:00'),
+('CTHD000000005-005', 'P024', 1, '2024-10-18 15:00:00');
+
+-- Chen du lieu vao bang LichSuDatDichVu
+INSERT INTO LichSuDatDichVu (maChiTietHoaDon, maDichVu, soLuongDatHang, thoiGianDatDichVu) VALUES
+('CTHD000000001-001', 'DV00000001-001', 2, '2024-10-14 15:00:00'),
+('CTHD000000001-001', 'DV00000004-004', 1, '2024-10-14 16:00:00'),
+('CTHD000000002-002', 'DV00000003-003', 1, '2024-10-15 13:00:00'),
+('CTHD000000003-003', 'DV00000002-002', 1, '2024-10-16 11:00:00'),
+('CTHD000000004-004', 'DV00000005-005', 1, '2024-10-17 12:00:00');
+
+-- Chen du lieu vao bang LoaiNhanVien
+INSERT INTO LoaiNhanVien (maLoaiNhanVien, tenLoaiNhanVien) VALUES
+('MLNV01', 'Nhan Vien'),
+('MLNV02', 'Quan Ly');
+
+
+-- Dữ liệu mẫu cho bảng NhanVien
+INSERT INTO NhanVien (maNhanVien, tenNhanVien, soDienThoai, gioiTinh, diaChi, ngaySinh, email, maLoaiNhanVien, trangThai) VALUES
+('NV001', 'Nguyen Cong Danh', '0123456789', 'NAM',  'Ha Noi', '1990-04-01', 'd@gmail.com', 'MLNV01', 'DANG_LAM_VIEC'),
+('NV003', 'Tran Quoc Bao', '0123456781', 'NAM',  'Ho Chi Minh', '1990-05-01', 'd1@gmail.com', 'MLNV02', 'DANG_LAM_VIEC'),
+('NV004', 'Dang Tran Tan Phat', '0923456781', 'NAM',  'Ho Chi Minh', '1991-04-01', 'd1@gmail.com', 'MLNV02', 'NGHI_PHEP'),
+('NV002', 'Huynh Thanh Liem', '7654321', 'NU', 'Da Nang', '1995-02-22', 'e@gmail.com', 'MLNV01', 'NGHI_VIEC');
+
+-- Dữ liệu mẫu cho bảng HoaDon
+INSERT INTO HoaDon (maHoaDon, ngayLapHoaDon, maNhanVien, maVoucher, maKhachHang, maChiTietHoaDon, VAT, trangThai, checkIN, checkOUT, datCoc, tienPhat, tongTien) VALUES
+('HD00000001-003', '2024-10-14', 'NV001', NULL, 'KH0001-001', 'CTHD000000001-001', 10, 1, 1, 0, 150000, 0, 900000),
+('HD00000001-004', '2024-10-15', 'NV002', 'VC2023101-01', 'KH0001-001', 'CTHD000000002-002', 10, 1, 1, 0, 200000, 0, 1100000),
+('HD00000001-005', '2024-10-16', 'NV001', NULL, 'KH0001-001', 'CTHD000000003-003', 10, 1, 1, 0, 250000, 0, 950000),
+('HD00000001-006', '2024-10-17', 'NV002', 'VC2023103-02', 'KH0002-002', 'CTHD000000004-004', 10, 1, 1, 0, 300000, 0, 1200000),
+('HD00000001-007', '2024-10-18', 'NV001', NULL, 'KH0001-001', 'CTHD000000005-005', 10, 1, 1, 0, 150000, 0, 1350000);
+
+
+-- Dữ liệu mẫu cho bảng TaiKhoan
+INSERT INTO TaiKhoan (tenTaiKhoan, matKhau, trangThai, maNhanVien) VALUES
+('danomo', '123', 'KHONG_HOAT_DONG', 'NV001'),
+('liem', '123', 'KHONG_HOAT_DONG', 'NV002'),
+('bao', '123', 'KHONG_HOAT_DONG', 'NV003'),
+('phat', '123', 'KHONG_HOAT_DONG', 'NV004');
+-- Dữ liệu ca làm việc
+INSERT INTO CaLamViec (maCaLamVien, tenCaLamViec, ngayLamviec, tongTienTrongCa, maNhanVien) VALUES
+('CA1', 'CA_SANG', '2024-10-10', 700000, 'NV001'),
+('CA2', 'CA_TRUA', '2024-10-10', 0, 'NV001'),
+('CA3', 'CA_TOI', '2024-10-10', 0, 'NV001'),
+('CA4', 'CA_SANG', '2024-10-11', 800000, 'NV002'),
+('CA5', 'CA_TRUA', '2024-10-11', 0, 'NV002'),
+('CA6', 'CA_TOI', '2024-10-11', 0, 'NV002'),
+('CA7', 'CA_SANG', '2024-10-14', 900000, 'NV001'),
+('CA8', 'CA_TRUA', '2024-10-14', 0, 'NV001'),
+('CA9', 'CA_SANG', '2024-10-15', 1100000, 'NV002'),
+('CA10', 'CA_TRUA', '2024-10-15', 0, 'NV002');
+
+
+
